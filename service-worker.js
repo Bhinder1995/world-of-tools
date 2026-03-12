a87f970dcce651fc5696a7ef13dd1b3db18188e7
Bhupinder singh <bhindersingh199529@gmail.com>
2026-03-12 11:36:12 +0530

    Fix: Restore wiped HTML files and apply unique content/versioning safely

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'world-of-tools-v74';
+const CACHE_NAME = 'world-of-tools-v75';
 
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
-    '/css/style.css?v=1.6',
-    '/js/common.js?v=1.6',
+    '/css/style.css?v=1.7',
+    '/js/common.js?v=1.7',
     '/logo.svg',
     '/manifest.json',
     '/app-icon.png',
